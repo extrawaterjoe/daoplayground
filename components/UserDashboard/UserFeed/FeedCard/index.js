eddf1096d3334b213633ee4ac452cce6058afe7a
joe lazarz <jlazarz87@gmail.com>
2022-04-18 22:16:46 -0400

    dao tx state

--- components/UserDashboard/UserFeed/FeedCard/index.js
+++ components/UserDashboard/UserFeed/FeedCard/index.js
@@ -6,7 +6,7 @@ const FeedCard = ({tx}) => {
     <div className="flex md:flex-row flex-col dark:bg-slate-800 bg-slate-200 space-x-2 rounded-xl p-3">
       <div className="rounded-full h-16 w-16 border"></div>
       <div className="flex flex-col">
-        <div className="text-xl">safe...</div>
+        <div className="text-xl">{tx?.txType}</div>
       </div>
     </div>
 
--- components/UserDashboard/UserFeed/index.js
+++ components/UserDashboard/UserFeed/index.js
@@ -1,17 +1,44 @@
 import React from "react"
+import Router from "next/router"
+import { useRouter } from "next/router"
 import FeedCard from "./FeedCard"
 
 import * as api from "/query"
-import { useQuery, useQueries } from "react-query"
-import { allDaoTx } from '/query/index';
+import { useQuery } from "react-query"
 
 const UserFeed = ({ data }) => {
-  const daos = data?.safes
+  const daos = React.useMemo(() => {
+    return data?.safes.map((dao, i) => (
+      <div className="dark:bg-slate-800 bg-slate-100 rounded-full p-1" onClick={() => setDao(dao)} key={i}>
+        {dao?.substring(0, 6) + "..." + dao.substring(dao.length - 4)}
+      </div>
+    ))
+  }, [data])
+
+  const [dao, setDao] = React.useState("")
+  const router = useRouter()
+
+  const reset = React.useCallback(() => {
+    setDao("false")
+  }, [setDao])
+
+  React.useEffect(() => {
+    router.events.on("routeChangeStart", reset)
+    return () => router.events.off("routeChangeStart", reset)
+  }, [reset, router.events])
+
+  const { data: txs, status } = useQuery(["txs", dao], () => api.allDaoTx(dao), {
+    enabled: dao !== "",
+    staleTime: 190000,
+  })
+
+  const daoTxs = txs?.results?.map((tx, i) => <FeedCard tx={tx} key={i} />)
 
   return (
     <div className="flex w-full flex-col p-3 lg:w-2/5 space-y-3">
       <div className="text-3xl">feed</div>
-        <FeedCard />
+      <div className="flex flex-row space-x-2 overflow-x-auto no-scrollbar">{daos}</div>
+      {daoTxs}
     </div>
   )
 }
