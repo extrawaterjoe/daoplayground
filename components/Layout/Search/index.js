6a891230b6c0368a8259a5852fd01c33eafbc06f
joe lazarz <jlazarz87@gmail.com>
2022-04-20 22:28:26 -0400

    search pg

--- components/Layout/Search/index.js
+++ components/Layout/Search/index.js
@@ -1,8 +1,92 @@
-import React from 'react'
+import React from "react"
+import axios from "axios"
+import { GoSearch } from "react-icons/go"
+import { useLayoutStore } from '/stores/useLayoutStore';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "fetch_start":
+      return {
+        ...state,
+        isLoading: true,
+        hasError: false,
+      }
+    case "fetch_success":
+      return {
+        ...state,
+        isLoading: false,
+        hasError: false,
+        hits: action.payload,
+      }
+    case "fetch_failure":
+      return {
+        ...state,
+        isLoading: false,
+        hasError: true,
+      }
+    default:
+      throw new Error()
+  }
+}
+
+const fetchHits = async (query, dispatch) => {
+  dispatch({ type: "fetch_start" })
+  try {
+    const result = await axios.post(`${process.env.NEXT_PUBLIC_API}/search`, {
+      query: query,
+    })
+    dispatch({ type: "fetch_success", payload: result.data })
+  } catch (err) {
+    dispatch({ type: "fetch_failure" })
+    axios.isCancel(err) || dispatch({ type: "fetch_failure" })
+  }
+}
 
 const Search = () => {
+  const [{ hits, hasError, isLoading }, dispatch] = React.useReducer(reducer, {
+    hits: [],
+    isLoading: true,
+    hasError: false,
+  })
+  const [query, setQuery] = React.useState("")
+
+  React.useEffect(() => {
+    const { cancel, token } = axios.CancelToken.source()
+    const timeOutId = setTimeout(() => fetchHits(query, dispatch, token), 500)
+    return () => cancel("No longer latest query") || clearTimeout(timeOutId)
+  }, [query])
+
+  React.useEffect(() => {
+    if (hits.length) {
+      console.log(hits)
+    }
+  }, [hits])
+
+  const setSearchOpen = useLayoutStore(state => state.setSearchOpen)
+
   return (
-    <div>Search</div>
+    <div className="flex flex-row w-full justify-center px-3">
+    <div className="relative md:w-1/2 w-full border-b-2 border-slate-300 py-3 text-slate-600 focus-within:text-slate-400 dark:focus-within:text-slate-100">
+      <span className="absolute left-0 top-4 flex items-center pl-2">
+        <GoSearch size={24} />
+      </span>
+      <input
+        autoFocus
+        className="w-full bg-slate-200 py-2 pl-12 text-sm text-white focus:text-slate-900 focus:outline-none dark:bg-slate-900 dark:focus:text-slate-100"
+        placeholder="Search..."
+        autoComplete="off"
+        onChange={event => setQuery(event.target.value)}
+        value={query}
+      />
+      <button
+        className="absolute right-2 top-3 rounded-lg border px-2 py-1 dark:text-white"
+        onClick={setSearchOpen}
+      >
+        esc
+      </button>
+    </div>
+    {/* RESULTS */}
+    </div>
   )
 }
 
