8d6d0380805b1011dd37536f385431dce9e48fed
joe lazarz <jlazarz87@gmail.com>
2022-04-22 14:24:19 -0400

    limit members to 10 on dao card and loading state for user addressEns

--- components/Playground/UserPanel/AddressEns.js
+++ components/Playground/UserPanel/AddressEns.js
@@ -8,12 +8,12 @@ const AddressEns = ({ address }) => {
   })
 
   if (ensLoading || !address) {
-    return <div className="flex w-full justify-center animate-pulse">loading</div>
+    return <div className="flex w-full justify-center h-10 bg-slate-200 dark:bg-slate-800 rounded-xl animate-pulse"></div>
   }
 
   return (
-    <div className="flex flex-row space-x-1 items-center justify-center w-full">
-      <div className="text-xl">{ensData ? ensData : address.substring(0, 6) + "..." + address.substring(address.length - 4)}</div>
+    <div className="flex flex-row space-x-1 h-10 items-center justify-center w-full">
+      <div className={(ensData && ensData.length > 18 ? "text-md" : "text-xl")}>{ensData ? ensData : address.substring(0, 6) + "..." + address.substring(address.length - 4)}</div>
       <button className="flex flex-row space-x-1 bg-slate-200 hover:border-teal-300 dark:bg-slate-800 dark:hover:bg-slate-700 border dark:border-slate-800 dark:hover:border-teal-300 text-xs p-2 rounded-full">
         <BiCopy size={16} />
         </button>
