c80c3f0642f630e4cea61f0a231185562f9d1369
joe lazarz <jlazarz87@gmail.com>
2022-04-22 10:22:06 -0400

    working on search in layout

--- components/Layout/Search/index.js
+++ components/Layout/Search/index.js
@@ -1,8 +1,10 @@
 import React from "react"
+import ResultsLoading from "./ResultsLoading"
+import UserResults from "./UserResults"
 import axios from "axios"
 import { GoSearch } from "react-icons/go"
 import { MdClose } from "react-icons/md"
-import { useLayoutStore } from '/stores/useLayoutStore';
+import { useLayoutStore } from "/stores/useLayoutStore"
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -66,27 +68,42 @@ const Search = () => {
   const setSearchOpen = useLayoutStore(state => state.setSearchOpen)
 
   return (
-    <div className="flex flex-row w-full justify-center px-3">
-    <div className="relative md:w-1/2 w-full border-b-2 border-slate-200 py-3 text-slate-600 focus-within:text-slate-400 dark:focus-within:text-slate-100">
-      <span className="absolute left-0 top-4 flex items-center pl-2">
-        <GoSearch size={24} />
-      </span>
-      <input
-        autoFocus
-        className="w-full bg-slate-300 py-2 pl-12 text-sm text-white focus:text-slate-900 focus:outline-none dark:bg-slate-900 dark:focus:text-slate-100"
-        placeholder="Search..."
-        autoComplete="off"
-        onChange={event => setQuery(event.target.value)}
-        value={query}
-      />
-      <button
-        className="absolute right-2 top-3 rounded-full border p-1 hover:dark:bg-slate-800 hover:bg-slate-100 dark:text-white"
-        onClick={setSearchOpen}
-      >
-        <MdClose size={24} />
-      </button>
-    </div>
-    {/* RESULTS */}
+    <div className="flex flex-col w-full justify-center items-center px-3">
+      {/* Search bar displays 1/2 width md breakpoint and full width on mobile*/}
+      <div className="relative md:w-1/2 w-full border-b-2 border-slate-200 py-3 text-slate-600 focus-within:text-slate-400 dark:focus-within:text-slate-100">
+        <span className="absolute left-0 top-4 flex items-center pl-2">
+          <GoSearch size={24} />
+        </span>
+        <input
+          autoFocus
+          className="w-full bg-slate-300 py-2 pl-12 text-sm text-white focus:text-slate-900 focus:outline-none dark:bg-slate-900 dark:focus:text-slate-100"
+          placeholder="Search..."
+          autoComplete="off"
+          onChange={event => setQuery(event.target.value)}
+          value={query}
+        />
+        <button
+          className="absolute right-2 top-3 rounded-full border p-1 hover:dark:bg-slate-800 hover:bg-slate-100 dark:text-white"
+          onClick={setSearchOpen}
+        >
+          <MdClose size={24} />
+        </button>
+      </div>
+      {/* RESULTS */}
+      {/* if there are hits in the search, pass them to Results */}
+      <div className="flex md:w-1/2 w-full">
+        {!query.length ? (
+          <>
+            <div className="flex h-24 w-full items-center justify-center">
+              <h1 className="font-semibold">start typing...</h1>
+            </div>
+          </>
+        ) : isLoading && query.length ? (
+          <ResultsLoading />
+        ) : hits?.Profiles?.length && query.length ? (
+          <UserResults hits={hits.Profiles} />
+        ) : null}
+      </div>
     </div>
   )
 }
